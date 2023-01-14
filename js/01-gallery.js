dde8bc50bab139935f87f31ea0f7c12e37a1c08c
IvanSlivenko <ivan8822@ulr.net>
2023-01-14 11:45:17 +0200

    etap-14-01-2023-11-45

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const currentGallery = document.querySelector('.gallery');
+
+//створимо розмітку
+function createGallery(masiv) {
+  return masiv.map((elem) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      data-source="${elem.original}"
+      alt="${elem.description}"
+    />
+  </a>
+</div>
+`;
+  
+})
+  
+  .join('');
+
+}
+
+const listGallery = createGallery(galleryItems);
+currentGallery.insertAdjacentHTML("beforeend", listGallery);
+
